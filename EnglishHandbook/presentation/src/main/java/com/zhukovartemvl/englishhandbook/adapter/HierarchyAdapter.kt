41a3b12ac9e11e269f87392a7169f5fa207683eb
Artyom Zhukov <zhukovartemvl@gmail.com>
2020-03-30 21:08:12 +0300

    исправление HierarchyAdapter

--- EnglishHandbook/presentation/src/main/java/com/zhukovartemvl/englishhandbook/adapter/HierarchyAdapter.kt
+++ EnglishHandbook/presentation/src/main/java/com/zhukovartemvl/englishhandbook/adapter/HierarchyAdapter.kt
@@ -1,6 +1,7 @@
 package com.zhukovartemvl.englishhandbook.adapter
 
 import android.graphics.Typeface
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
@@ -16,7 +17,10 @@ import com.zhukovartemvl.englishhandbook.adapter.base.BaseViewHolder
 
 class HierarchyAdapter : BaseAdapter<Category>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<Category> {
-        TODO("Not yet implemented")
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_hierarchy, parent, false),
+            callback = mCallback
+        )
     }
 
     class ViewHolder(itemView: View, var callback: BaseAdapterCallback<Category>?) :
@@ -25,7 +29,7 @@ class HierarchyAdapter : BaseAdapter<Category>() {
         private val background: ConstraintLayout = itemView.findViewById(R.id.item_background)
         private val txtTitle: TextView = itemView.findViewById(R.id.txt_title)
         private val imgDot: ImageView = itemView.findViewById(R.id.img_dot)
-        private val imgIcon: ImageView = itemView.findViewById(R.id.img_folder)
+        private val imgIcon: ImageView = itemView.findViewById(R.id.img_icon)
         private val imgArrow: ImageView = itemView.findViewById(R.id.img_arrow)
 
         override fun bind(model: Category) {
@@ -38,11 +42,18 @@ class HierarchyAdapter : BaseAdapter<Category>() {
                     imgArrow.visibility = View.GONE
                     imgIcon.setImageResource(R.drawable.ic_back)
                 }
-                is HeaderItem -> {
+                is TitleItem -> {
                     val backgroundColor = ContextCompat.getColor(itemView.context, R.color.header)
                     background.setBackgroundColor(backgroundColor)
                     txtTitle.setText(model.title)
                     txtTitle.setTypeface(null, Typeface.BOLD)
+                    imgDot.visibility = View.GONE
+                    imgArrow.visibility = View.GONE
+                }
+                is SubtitleItem -> {
+                    val backgroundColor = ContextCompat.getColor(itemView.context, R.color.header)
+                    background.setBackgroundColor(backgroundColor)
+                    txtTitle.setText(model.title)
                     imgDot.visibility = View.GONE
                     imgArrow.visibility = View.GONE
                 }
