f4dde43ff78ee15f4897b6d3629a2b3fa736a2a3
Artyom Zhukov <zhukovartemvl@gmail.com>
2020-04-25 10:55:16 +0300

    Удалил лишний модуль, поправил Koin ViewModel

--- EnglishHandbook/feature/article/src/main/java/com/zhukovartemvl/article/di/FeatureArticleModule.kt
+++ EnglishHandbook/feature/article/src/main/java/com/zhukovartemvl/article/di/FeatureArticleModule.kt
@@ -2,7 +2,7 @@ package com.zhukovartemvl.article.di
 
 import com.zhukovartemvl.article.ArticleViewModel
 import org.koin.dsl.module
-import org.koin.android.viewmodel.ext.koin.viewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 
 
 val featureArticleModule = module {
