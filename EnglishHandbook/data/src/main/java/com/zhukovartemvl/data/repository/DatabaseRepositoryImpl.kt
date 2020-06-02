a55587f7bc391e8bb8968d2d896191f884469829
Artyom Zhukov <zhukovartemvl@gmail.com>
2020-06-02 10:15:28 +0300

    Удалил тесты, обновил gradle и версию Android Studio, добавил тестовую html статью

--- EnglishHandbook/data/src/main/java/com/zhukovartemvl/data/repository/DatabaseRepositoryImpl.kt
+++ EnglishHandbook/data/src/main/java/com/zhukovartemvl/data/repository/DatabaseRepositoryImpl.kt
@@ -12,11 +12,9 @@ class DatabaseRepositoryImpl(private val database: DatabaseLoader) : DatabaseRep
     override fun initDatabase(context: Context): Boolean {
         return database.init(context)
     }
-
     override fun updateAvailable(): Boolean {
         return database.updateAvailable()
     }
-
     override fun updateDatabase(context: Context): Boolean {
         return database.updateDatabase(context)
     }
@@ -29,25 +27,19 @@ class DatabaseRepositoryImpl(private val database: DatabaseLoader) : DatabaseRep
     override fun getDatabaseParameter(): DatabaseParametersItem {
         return database.instance.parameterDao().getParameters.toDatabaseParametersItem()
     }
-
     override fun getArticle(key: String): ArticleItem {
         return database.instance.articleDao().getByKey(key).toArticleItem()
     }
-
     override fun getDictionary(key: String): List<DictionaryItem> {
         return database.instance.dictionaryDao().getByKey(key).map { it.toDictionaryItem() }
     }
-
     override fun getVerbs(key: String): List<IrregularVerbItem> {
         return database.instance.verbDao().getByKey(key).map { it.toIrregularVerbItem() }
     }
-
     override fun getVerbs(): List<IrregularVerbItem> {
         return database.instance.verbDao().getAll().map { it.toIrregularVerbItem() }
     }
-
     override fun getLinks(key: String): List<LinkItem> {
         return database.instance.linkDao().getByKey(key).map { it.toLinkItem() }
     }
-
 }
